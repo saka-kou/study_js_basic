e4b98b6f9822811cb4a11a554f0a061c0f0e33ff
坂口　晃平 <2250399@ecc.ac.jp>
2025-12-02 16:19:11 +0900

    function完成

--- 06_function/js/function06_practice.js
+++ 06_function/js/function06_practice.js
@@ -1,21 +1,22 @@
-const btnA = document.getElementById("btnA");
-const btnB = document.getElementById("btnB");
-const btnC = document.getElementById("btnC");
+// const btnA = document.getElementById("btnA");
+// const btnB = document.getElementById("btnB");
+// const btnC = document.getElementById("btnC");
+// let btnName = ["btnA", "btnB", "btnC"];
+// let btn = [btnA, btnB, btnC];
+// console.dir(btns[0]);
+const btns = document.querySelectorAll(".btn");
 const result = document.getElementById("result");
 
-let btn = [btnA, btnB, btnC];
-let btnName = ["btnA", "btnB", "btnC"]
 
 function message(msg) {
   result.innerText = `${msg}が押された`;
 }
 
-for (let i = 0; i < btn.length; i++) {
-  btn[i].addEventListener(`click`, () => {
-    message(btnName[i]);
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener(`click`, () => {
+    message(btns[i].textContent);
   })
 }
-
 
 // btnA.onclick = () => {
 //   result.innerText = "btnAが押された";
