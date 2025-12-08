6351accbbbfa46bc13a81d1e4bcc75d29da47bc1
坂口　晃平 <2250399@ecc.ac.jp>
2025-12-08 17:08:31 +0900

    timer途中

--- 08_timer/js/timer03.js
+++ 08_timer/js/timer03.js
@@ -1,1 +1,13 @@
-const arr = ["HTML","CSS","JavaScript","Pug","Sass","TypeScript"];
+const arr = ["HTML", "CSS", "JavaScript", "Pug", "Sass", "TypeScript"];
+const skill = document.querySelector("#skill");
+let count = 0;
+
+setInterval(() => {
+    if (count < arr.length) {
+        skill.innerText += arr[count];
+        count++
+    }
+    else {
+        return false
+    }
+}, 3000);
---
+++ 08_timer/js/timer04.js
@@ -0,0 +1,10 @@
+const start = document.querySelector("#startBtn");
+const stop = document.querySelector("#stoptBtn");
+const reset = document.querySelector("#resetBtn");
+let timerId;
+let num = 0;
+
+start.addEventListener(`click`, () => {
+
+});
+
