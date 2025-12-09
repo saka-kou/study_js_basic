2c1a087a9c92c08035fc57b4b507757bff6a905f
坂口　晃平 <2250399@ecc.ac.jp>
2025-12-09 16:57:06 +0900

    timer途中

--- 08_timer/js/timer04.js
+++ 08_timer/js/timer04.js
@@ -1,10 +1,29 @@
 const start = document.querySelector("#startBtn");
-const stop = document.querySelector("#stoptBtn");
+const stopBtn = document.querySelector("#stopBtn");
 const reset = document.querySelector("#resetBtn");
+const out = document.querySelector("#out");
 let timerId;
 let num = 0;
 
 start.addEventListener(`click`, () => {
+    if (!timerId) {
+        timerId = setInterval(() => {
+            num++;
+            console.log("たいまー");
+            out.innerText = num;
+        }, 1000);
+    }
+})
 
-});
+stopBtn.addEventListener(`click`, () => {
+    console.log("すとっぷー");
+    clearInterval(timerId);
+    timerId = null;
+})
 
+reset.addEventListener(`click`, () => {
+    clearInterval(timerId);
+    timerId = null;
+    num = 0;
+    out.innerText = num;
+})
